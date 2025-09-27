758bfcc0359231af4ace5d37d174f1922b2cc705
DivineDemon <supame123@gmail.com>
2025-09-27 19:16:46 +0500

    Reversion.

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,5 +1,6 @@
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
 	hello: publicProcedure
@@ -9,4 +10,22 @@ export const postRouter = createTRPCRouter({
 				greeting: `Hello ${input.text}`,
 			};
 		}),
+
+	create: publicProcedure
+		.input(z.object({ name: z.string().min(1) }))
+		.mutation(async ({ ctx, input }) => {
+			return ctx.db.post.create({
+				data: {
+					name: input.name,
+				},
+			});
+		}),
+
+	getLatest: publicProcedure.query(async ({ ctx }) => {
+		const post = await ctx.db.post.findFirst({
+			orderBy: { createdAt: "desc" },
+		});
+
+		return post ?? null;
+	}),
 });
